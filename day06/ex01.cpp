75ec1a09b22a625eb6074c1eb167ce884c84bbbd
nherimam <herimaminavalonaprosper@gmail.com>
2025-12-09 14:08:53 +0300

    Part one of day five finished

---
+++ day06/ex01.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(int ac, char **av) {
+    double  sum = 0;
+    string  s;
+    fstream fs(av[1]);
+    vector<char> op;
+    vector<vector<int>> tab;
+
+
+    /*                 RECUPERATION DE DONNEE                 */
+    while (getline(fs, s)) {
+        vector<int> one;
+        stringstream    ts(s);
+        stringstream    ss(s);
+        ts >> s;
+        if (s.compare("+") && s.compare("*")) {
+            int val;
+            while (ss >> val) {
+                one.push_back(val);
+            }
+            tab.push_back(one);
+        } else {
+            char    c;
+            while (ss >> c) {
+                op.push_back(c);
+            }
+        }
+    }
+
+
+    /*                 TRAITEMENT DE DONNEE                 */
+    for (size_t i = 0; i < op.size(); i ++) {
+        if (op[i] == '+') {
+            double sumi = 0;
+            for (size_t j = 0; j < tab.size(); j ++) {
+                sumi += tab[j][i];
+            }
+            sum += sumi;
+        } else {
+            double muli = 1;
+            for (size_t j = 0; j < tab.size(); j ++) {
+                muli *= tab[j][i];
+            }
+            sum += muli;
+        }
+    }
+    cout << fixed << setprecision(0) << sum << endl;
+}
